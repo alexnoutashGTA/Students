a8c337216202a514a684c36472c453fd32f04edf
Alex Noutash <alnoutash@hotmail.com>
2025-06-02 13:15:30 -0400

    Side nav component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { SideMenu } from './side-menu/side-menu';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    SideMenu
   ],
   imports: [
     BrowserModule,
