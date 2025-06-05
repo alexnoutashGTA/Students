9b54312cdac95592db22db15c36e3bd3e2a5dc4f
Alex Noutash <alnoutash@hotmail.com>
2025-06-05 11:00:28 -0400

    wip

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {Home} from './home/home';
 import {Messages} from './messages/messages';
 import {Detail} from './detail/detail';
