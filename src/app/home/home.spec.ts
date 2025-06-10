43b99c6e7672043a5daacf9183ec600cc21b53f4
Alex Noutash <alnoutash@hotmail.com>
2025-06-10 11:30:22 -0400

    disabled component tests for now

--- src/app/home/home.spec.ts
+++ src/app/home/home.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Home } from './home';
 
-describe('Home', () => {
+xdescribe('Home', () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
 
