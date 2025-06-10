73d18accbe96fcf0e6fd61bf2019378f445ecdd6
Alex Noutash <alnoutash@hotmail.com>
2025-06-10 13:29:02 -0400

    Array function

--- src/app/services/main-service.ts
+++ src/app/services/main-service.ts
@@ -4,20 +4,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MainService {
-
+  ImagesLinks: string[]=[]
 
   constructor() {
-  }
+     this.ImagesLinks= [
+      "https://randomuser.me/api/portraits/men/1.jpg",
+      "https://randomuser.me/api/portraits/women/2.jpg",
+      "https://randomuser.me/api/portraits/men/3.jpg",
+      "https://randomuser.me/api/portraits/women/4.jpg",
+      "https://randomuser.me/api/portraits/men/5.jpg",
+      "https://randomuser.me/api/portraits/men/6.jpg",
+      "https://randomuser.me/api/portraits/men/7.jpg",
+      "https://randomuser.me/api/portraits/men/8.jpg",
+      "https://randomuser.me/api/portraits/men/9.jpg",
+      "https://randomuser.me/api/portraits/men/10.jpg"];
 
-  ImagesLinks: string[] = [
-    "https://randomuser.me/api/portraits/men/1.jpg",
-    "https://randomuser.me/api/portraits/women/2.jpg",
-    "https://randomuser.me/api/portraits/men/3.jpg",
-    "https://randomuser.me/api/portraits/women/4.jpg",
-    "https://randomuser.me/api/portraits/men/5.jpg",
-    "https://randomuser.me/api/portraits/men/6.jpg",
-    "https://randomuser.me/api/portraits/men/7.jpg",
-    "https://randomuser.me/api/portraits/men/8.jpg",
-    "https://randomuser.me/api/portraits/men/9.jpg",
-    "https://randomuser.me/api/portraits/men/10.jpg"]
+     this.ImagesLinks.push( "https://randomuser.me/api/portraits/men/11.jpg");
+    this.ImagesLinks.push( "https://randomuser.me/api/portraits/men/12.jpg");
+    this.ImagesLinks.push( "https://randomuser.me/api/portraits/men/13.jpg");
+    this.ImagesLinks.push( "https://randomuser.me/api/portraits/men/15.jpg");
+
+    this.ImagesLinks.splice(5, 0, "https://randomuser.me/api/portraits/men/16.jpg");
+    console.log("Total length: "+this.ImagesLinks.length);
+   this.ImagesLinks.forEach(link => {
+     console.log(link);
+   });
+  }
 }
+
