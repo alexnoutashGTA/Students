e412a38503fa735c6a05b96cab0c0f302d9f6c12
Alex Noutash <alnoutash@hotmail.com>
2025-06-12 10:45:44 -0400

    App to SideMenu Input

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class App {
   protected title = 'MyBook';
+  sideMenuTitle: string = 'Welcome to MyBook!';
 }
--- src/app/side-menu/side-menu.ts
+++ src/app/side-menu/side-menu.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {MenuService} from '../services/menu-service';
 
 @Component({
@@ -8,6 +8,8 @@ import {MenuService} from '../services/menu-service';
   styleUrl: './side-menu.css'
 })
 export class SideMenu {
+  @Input() title = ''; // decorate the property with @Input()
+
   homeLabel: String = "";
   messagesLabel: String = "";
   detailsLabel: String = "";
