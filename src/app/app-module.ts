bb8ade2bffb099e432d2baa432bd264fb6396603
Tmac0413 <theresa.cc204165@myciticollege.ca>
2025-06-17 13:27:34 -0400

    Added Validators to imports

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,6 +1,5 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { SideMenu } from './side-menu/side-menu';
@@ -9,7 +8,7 @@ import { Messages } from './messages/messages';
 import { Detail } from './detail/detail';
 import {BreadCrumbMenu} from './bread-crumb-menu/bread-crumb-menu';
 import { Profile } from './profile/profile';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @NgModule({
   declarations: [
