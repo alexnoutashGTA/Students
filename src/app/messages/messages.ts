f43b0842286231af77398845c0e1ed4eb31a702e
Tmac0413 <theresa.cc204165@myciticollege.ca>
2025-06-18 11:10:45 -0400

    updated validation

--- src/app/messages/messages.ts
+++ src/app/messages/messages.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
 =======
 import {messageObject} from './messageObject';
@@ -8,13 +9,26 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvider';
 >>>>>>> 9d7ff11 (Changes made to form so far)
 =======
+=======
+>>>>>>> 9303344 (updated validation)
 import {messageObject} from './messageObject';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvider';
 =======
 import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
 >>>>>>> 1b32d4d (validation)
+<<<<<<< HEAD
 >>>>>>> 47b1980 (validation)
+=======
+=======
+import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
+=======
+import {messageObject} from './messageObject';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvider';
+>>>>>>> 111b64c (updated validation)
+>>>>>>> 0e8b983 (updated validation)
+>>>>>>> 9303344 (updated validation)
 
 @Component({
   selector: 'app-messages',
@@ -28,12 +42,15 @@ export class Messages {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   submitForm=new FormGroup({
 =======
 =======
 >>>>>>> 47b1980 (validation)
 =======
 >>>>>>> 7fe1def (fixed unit test)
+=======
+>>>>>>> 9303344 (updated validation)
   profileForm = new FormGroup({
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -96,20 +113,43 @@ export class Messages {
 =======
   submitForm:FormGroup=new FormGroup({
 =======
+=======
+>>>>>>> 0e8b983 (updated validation)
   submitForm=new FormGroup({
 >>>>>>> 7a209af (fixed unit test)
 
     userName:new FormControl("",[Validators.required]),
     message: new FormControl("",[Validators.required]),
     date:    new FormControl("",[Validators.required]),
+=======
+  profileForm = new FormGroup({
+   userName: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+    message: new FormControl('', Validators.required,),
+    date: new FormControl('', Validators.required),
+    })
+
+
+
+
+>>>>>>> 111b64c (updated validation)
 
   });
 
+<<<<<<< HEAD
   onUserSave(){
     const formValue=this.submitForm.value;
     console.log(formValue);
+<<<<<<< HEAD
 >>>>>>> 1b32d4d (validation)
+<<<<<<< HEAD
 >>>>>>> 47b1980 (validation)
+=======
+=======
+=======
+    console.log('Form submitted'+" "+this.profileForm.controls['userName'].value+" "+this.profileForm.controls['message'].value);
+>>>>>>> 111b64c (updated validation)
+>>>>>>> 0e8b983 (updated validation)
+>>>>>>> 9303344 (updated validation)
   }
 
 }
