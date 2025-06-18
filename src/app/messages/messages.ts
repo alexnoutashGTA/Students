93033446c9523e7574a5b274ec516574191b1302
Tmac0413 <theresa.cc204165@myciticollege.ca>
2025-06-18 11:10:45 -0400

    updated validation

--- src/app/messages/messages.ts
+++ src/app/messages/messages.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import {messageObject} from './messageObject';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvider';
 =======
 import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
 >>>>>>> 1b32d4d (validation)
+=======
+import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
+=======
+import {messageObject} from './messageObject';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvider';
+>>>>>>> 111b64c (updated validation)
+>>>>>>> 0e8b983 (updated validation)
 
 @Component({
   selector: 'app-messages',
@@ -15,6 +24,7 @@ import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/fo
 })
 export class Messages {
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
   profileForm = new FormGroup({
@@ -33,19 +43,39 @@ export class Messages {
 =======
   submitForm:FormGroup=new FormGroup({
 =======
+=======
+>>>>>>> 0e8b983 (updated validation)
   submitForm=new FormGroup({
 >>>>>>> 7a209af (fixed unit test)
 
     userName:new FormControl("",[Validators.required]),
     message: new FormControl("",[Validators.required]),
     date:    new FormControl("",[Validators.required]),
+=======
+  profileForm = new FormGroup({
+   userName: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+    message: new FormControl('', Validators.required,),
+    date: new FormControl('', Validators.required),
+    })
+
+
+
+
+>>>>>>> 111b64c (updated validation)
 
   });
 
+<<<<<<< HEAD
   onUserSave(){
     const formValue=this.submitForm.value;
     console.log(formValue);
+<<<<<<< HEAD
 >>>>>>> 1b32d4d (validation)
+=======
+=======
+    console.log('Form submitted'+" "+this.profileForm.controls['userName'].value+" "+this.profileForm.controls['message'].value);
+>>>>>>> 111b64c (updated validation)
+>>>>>>> 0e8b983 (updated validation)
   }
 
 }
