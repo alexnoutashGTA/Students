5ba640ce281955864dc4fd41c5f39ab8806ab965
Alex Noutash <alnoutash@hotmail.com>
2025-06-18 12:43:45 -0400

    Sample Forms Test

--- src/app/messages/messages.spec.ts
+++ src/app/messages/messages.spec.ts
@@ -1,3 +1,5 @@
+import {FormGroup, ReactiveFormsModule} from '@angular/forms';
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Messages } from './messages';
@@ -20,7 +22,7 @@ describe('Messages', () => {
       ]
 
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Messages);
     component = fixture.componentInstance;
