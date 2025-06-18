d29c398e6ff3a9ec2685b5d7b9fad1a372f60613
Alex Noutash <alnoutash@hotmail.com>
2025-06-18 12:43:45 -0400

    Sample Forms Test

--- src/app/messages/messages.spec.ts
+++ src/app/messages/messages.spec.ts
@@ -3,6 +3,15 @@ import {FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Messages } from './messages';
+<<<<<<< HEAD
+=======
+import {CommonModule} from '@angular/common';
+<<<<<<< HEAD
+import {ReactiveFormsModule} from '@angular/forms';
+=======
+>>>>>>> 1edb8c3 (Sample Forms Test)
+import {AppModule} from '../app-module';
+>>>>>>> 2418a27 (Sample Forms Test)
 
 describe('Messages', () => {
   let component: Messages;
@@ -10,7 +19,22 @@ describe('Messages', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+<<<<<<< HEAD
       declarations: [Messages]
+=======
+      declarations: [Messages],
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AppModule
+<<<<<<< HEAD
+
+      ]
+
+=======
+      ]
+>>>>>>> 1edb8c3 (Sample Forms Test)
+>>>>>>> 2418a27 (Sample Forms Test)
     })
       .compileComponents();
 
@@ -25,6 +49,7 @@ describe('Messages', () => {
 <<<<<<< HEAD
 =======
 
+<<<<<<< HEAD
 it('First Form Test', () => {
   let ourFormsGroup = component.submitForm;
   console.log("ourFormsGroup");
@@ -32,5 +57,16 @@ it('First Form Test', () => {
   expect(ourFormsGroup.controls.userName.value).toEqual('');
 })
 
+<<<<<<< HEAD
 >>>>>>> da09f78 (changes)
+=======
+=======
+  it('First Form Test', () => {
+    let ourFormsGroup = component.profileForm;
+    console.log("ourFormsGroup");
+    console.log(ourFormsGroup);
+    expect(ourFormsGroup.controls.userName.value).toEqual('Alex')
+  })
+>>>>>>> 1edb8c3 (Sample Forms Test)
+>>>>>>> 2418a27 (Sample Forms Test)
 });
--- src/app/messages/messages.ts
+++ src/app/messages/messages.ts
@@ -19,9 +19,16 @@ export class Messages {
   submitForm=new FormGroup({
 =======
   profileForm = new FormGroup({
+<<<<<<< HEAD
    userName: new FormControl(''),
     message: new FormControl(''),
     date: new FormControl(''),
+=======
+   userName: new FormControl('Alex', [Validators.required, Validators.maxLength(6)]),
+    message: new FormControl('', Validators.required,),
+    date: new FormControl('', Validators.required),
+    })
+>>>>>>> 1edb8c3 (Sample Forms Test)
 
 });
   formMessage: messageObject = new messageObject("", "");
@@ -33,9 +40,16 @@ export class Messages {
 
   });
 
+<<<<<<< HEAD
   onUserSave(){
     const formValue=this.submitForm.value;
     console.log(formValue);
+=======
+
+  submitForm(event: Event) {
+    event.preventDefault();
+    console.log('Form submitted'+" "+this.profileForm.controls['userName'].value+" "+this.profileForm.controls['message'].value);
+>>>>>>> 1edb8c3 (Sample Forms Test)
   }
 
 }
