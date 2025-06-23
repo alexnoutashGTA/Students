39a96db5034b5f53c3e6cc734decb8dc2d8d1bb2
Alex Noutash <alnoutash@hotmail.com>
2025-06-23 12:39:43 -0400

    Resolved Conflicts

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,13 +5,8 @@ import { App } from './app';
 import { SideMenu } from './side-menu/side-menu';
 import { Home } from './home/home';
 import {BreadCrumbMenu} from './bread-crumb-menu/bread-crumb-menu';
-<<<<<<< HEAD
-import {Profile} from './profile/profile';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-=======
 import { Profile } from './profile/profile';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
->>>>>>> 9d7ff11 (Changes made to form so far)
 
 @NgModule({
   declarations: [
@@ -24,12 +19,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
   ],
     imports: [
         BrowserModule,
-<<<<<<< HEAD
-        AppRoutingModule,FormsModule,ReactiveFormsModule,
-=======
         AppRoutingModule,
         FormsModule, ReactiveFormsModule,
->>>>>>> 9d7ff11 (Changes made to form so far)
     ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/messages/messages.ts
+++ src/app/messages/messages.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import {FormControl, FormGroup,Validators,ReactiveFormsModule} from '@angular/forms';
-=======
 import {messageObject} from './messageObject';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvider';
->>>>>>> 9d7ff11 (Changes made to form so far)
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-messages',
@@ -15,41 +10,15 @@ import {dateTimestampProvider} from 'rxjs/internal/scheduler/dateTimestampProvid
 })
 export class Messages {
 
-<<<<<<< HEAD
-  submitForm=new FormGroup({
-=======
-  profileForm = new FormGroup({
-<<<<<<< HEAD
-   userName: new FormControl(''),
-    message: new FormControl(''),
-    date: new FormControl(''),
-=======
+  submitForm = new FormGroup({
    userName: new FormControl('Alex', [Validators.required, Validators.maxLength(6)]),
     message: new FormControl('', Validators.required,),
     date: new FormControl('', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]),
     })
->>>>>>> 1edb8c3 (Sample Forms Test)
 
-});
-  formMessage: messageObject = new messageObject("", "");
->>>>>>> 9d7ff11 (Changes made to form so far)
-
-    userName:new FormControl("",[Validators.required]),
-    message: new FormControl("",[Validators.required]),
-    date:    new FormControl("",[Validators.required]),
-
-  });
-
-<<<<<<< HEAD
-  onUserSave(){
-    const formValue=this.submitForm.value;
+  onUserSave() {
+    const formValue = this.submitForm.value;
     console.log(formValue);
-=======
-
-  submitForm(event: Event) {
-    event.preventDefault();
-    console.log('Form submitted'+" "+this.profileForm.controls['userName'].value+" "+this.profileForm.controls['message'].value);
->>>>>>> 1edb8c3 (Sample Forms Test)
   }
 
 }
