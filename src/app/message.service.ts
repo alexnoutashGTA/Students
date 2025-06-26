43cd03dbe0eac12ffc984ea49f5943e2e2c6eb17
Amardip Singh <amardip.cc203636@myciticollege.ca>
2025-06-26 14:57:45 -0700

    Implemented message form, persistence, and display in Agular project

---
+++ src/app/message.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  private messages: string[] = [];
+
+  addMessage(message: string) {
+    this.messages.push(message);
+  }
+
+  getMessages(): string[] {
+    return this.messages;
+  }
+}
