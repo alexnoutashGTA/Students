fc0e501f8d92a3160e2c8ec7c9ba66bb8f0e1d89
Amardip Singh <amardip.cc203636@myciticollege.ca>
2025-06-26 16:42:59 -0700

    fix: restore MessagesComponent with proper implementation

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,5 +1,22 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
+export class MessagesComponent {
+  newMessage: string = '';
+  messages: string[] = [];
+
+  constructor(private messageService: MessageService) {}
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.messageService.addMessage(this.newMessage);
+      this.messages = this.messageService.getMessages();
+      this.newMessage = '';
+    }
+  }
+}
