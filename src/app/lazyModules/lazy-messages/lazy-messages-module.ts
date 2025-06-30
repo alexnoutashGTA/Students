dbbddcdabbb5230da992eb4247b577a118ec28da
Alex Noutash <alnoutash@hotmail.com>
2025-06-30 13:35:33 -0400

    improved layout

--- src/app/lazyModules/lazy-messages/lazy-messages-module.ts
+++ src/app/lazyModules/lazy-messages/lazy-messages-module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import { LazyMessagesRoutingModule } from './lazy-messages-routing-module';
 import {Messages} from '../../messages/messages';
@@ -10,9 +10,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   declarations: [
     Messages
   ],
-  imports: [
-    CommonModule,
-    LazyMessagesRoutingModule,FormsModule,ReactiveFormsModule,
-  ]
+    imports: [
+        CommonModule,
+        LazyMessagesRoutingModule, FormsModule, ReactiveFormsModule, NgOptimizedImage,
+    ]
 })
 export class LazyMessagesModule { }
