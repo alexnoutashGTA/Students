39cdb392f654d086ae702a86e61c493cd048e6e1
Muhammad Javeed <muhammad.cc204754@myciticollege.ca>
2025-07-03 02:52:55 +0500

    Updated assignment for Waleed Rehman

--- my-angular-project/src/app/app.ts
+++ my-angular-project/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './messages.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'my-angular-project';
+  public messages: string[] = [];
+  public newMessage: string = '';
+
+  constructor(private messageService: MessageService) {
+    this.messages = this.messageService.getMessages();
+  }
+
+  addMessage() {
+    if (this.newMessage.trim()) {
+      this.messageService.addMessage(this.newMessage);
+      this.newMessage = '';
+      this.messages = this.messageService.getMessages();
+    }
+  }
 }
---
+++ my-angular-project/src/app/message.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  private messages: string[] = [];
+
+  getMessages(): string[] {
+    return this.messages;
+  }
+
+  addMessage(message: string): void {
+    this.messages.push(message);
+  }
+} 
