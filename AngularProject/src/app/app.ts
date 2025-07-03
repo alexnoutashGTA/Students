89477ea9934a2547ce10afb97b7012cadb19edb0
Muhammad Javeed <muhammad.cc204754@myciticollege.ca>
2025-07-03 19:43:09 +0500

    Added assignment for Muhammad Javeed

--- AngularProject/src/app/app.ts
+++ AngularProject/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MessageService } from './message.service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './messages.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'AngularProject';
+  newMessage = '';
+  messages: string[] = [];
+
+  constructor(private messageService: MessageService) {
+    this.messages = this.messageService.getMessages();
+  }
+
+  sendMessage() {
+    if (this.newMessage.trim()) {
+      this.messageService.addMessage(this.newMessage.trim());
+      this.newMessage = '';
+      this.messages = this.messageService.getMessages();
+    }
+  }
 }
