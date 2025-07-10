18298e056b0973b15f11af6882a7670acca31a3c
Alex Noutash <alnoutash@hotmail.com>
2025-07-10 11:14:44 -0400

    Accessible Images

--- src/app/services/main-service.ts
+++ src/app/services/main-service.ts
@@ -5,7 +5,7 @@ import {messageObject} from "../messages/messageObject";
   providedIn: 'root'
 })
 export class MainService {
-  ImagesLinks: string[] = []
+  ImagesLinks: { url: string; desc: string }[] = []
 
   HomePageNumber: number = 0;
 
@@ -28,23 +28,85 @@ export class MainService {
 
   constructor() {
     this.ImagesLinks = [
-      "https://randomuser.me/api/portraits/men/1.jpg",
-      "https://randomuser.me/api/portraits/women/2.jpg",
-      "https://randomuser.me/api/portraits/men/3.jpg",
-      "https://randomuser.me/api/portraits/women/4.jpg",
-      "https://randomuser.me/api/portraits/men/5.jpg",
-      "https://randomuser.me/api/portraits/men/6.jpg",
-      "https://randomuser.me/api/portraits/men/7.jpg",
-      "https://randomuser.me/api/portraits/men/8.jpg",
-      "https://randomuser.me/api/portraits/men/9.jpg",
-      "https://randomuser.me/api/portraits/men/10.jpg"];
-
-    this.ImagesLinks.push("https://randomuser.me/api/portraits/men/11.jpg");
-    this.ImagesLinks.push("https://randomuser.me/api/portraits/men/12.jpg");
-    this.ImagesLinks.push("https://randomuser.me/api/portraits/men/13.jpg");
-    this.ImagesLinks.push("https://randomuser.me/api/portraits/men/15.jpg");
-
-    this.ImagesLinks.splice(5, 0, "https://randomuser.me/api/portraits/men/16.jpg");
-
+      {
+        url: "https://randomuser.me/api/portraits/men/1.jpg",
+        desc: "First Man Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/1.jpg",
+        desc: "First Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/2.jpg",
+        desc: "Second Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/2.jpg",
+        desc: "Second Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/3.jpg",
+        desc: "Third Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/3.jpg",
+        desc: "Third Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/4.jpg",
+        desc: "Forth Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/4.jpg",
+        desc: "Forth Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/5.jpg",
+        desc: "Fifth Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/5.jpg",
+        desc: "Fifth Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/6.jpg",
+        desc: "Sixth Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/6.jpg",
+        desc: "Sixth Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/7.jpg",
+        desc: "Seventh Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/7.jpg",
+        desc: "Seventh Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/8.jpg",
+        desc: "Eight Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/8.jpg",
+        desc: "Eight Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/9.jpg",
+        desc: "Ninth Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/9.jpg",
+        desc: "Ninth Woman Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/men/10.jpg",
+        desc: "Tenth Men Image"
+      },
+      {
+        url: "https://randomuser.me/api/portraits/women/10.jpg",
+        desc: "Tenth Woman Image"
+      }]
   }
 }
