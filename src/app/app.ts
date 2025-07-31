8c620354eb01c2cfe99cc277d6dd0c16ed4512bb
Alex Noutash <alnoutash@hotmail.com>
2025-07-31 11:41:12 -0400

    angular page view for analytics

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs';
+declare var gtag: Function; // Declare gtag to avoid TypeScript errors
 
 @Component({
   selector: 'app-root',
@@ -6,13 +9,32 @@ import {Component, Input} from '@angular/core';
   standalone: false,
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'MyBook';
   breadCrumbTitle: string = 'Our Community App!';
   sideMenuTitle: string = 'Our Community App!';
 
   menuItemClicked($event: any) {
     console.log($event)
+  }
+  constructor(private router: Router) {}
+
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Send pageview event to Google Analytics
+      gtag('event', 'page_view', {
+        page_path: event.urlAfterRedirects,
+        page_title: this.getPageTitle(event.urlAfterRedirects) // Implement logic to get dynamic page title
+      });
+    });
+  }
+  private getPageTitle(url: string): string {
+    // Implement logic to extract or determine the page title from the URL or route configuration
+    // For example, you might have a data property in your route config for the title
+    return 'MyBook';
   }
 }
 
