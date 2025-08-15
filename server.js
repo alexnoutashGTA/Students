390601d2ff4b0a26e13ed4a50481e3aba1d388a9
Alex Noutash <alnoutash@hotmail.com>
2025-08-15 02:27:54 -0400

    port

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
-app.use(cors());
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser');
 
 // Sample data: a FAQ list in JSON format
